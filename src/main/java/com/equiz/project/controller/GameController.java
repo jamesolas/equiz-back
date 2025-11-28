ab96a28a202df557739a773c8b95a69f90257c8c
jamesolas <jamesolas@gmail.com>
2025-11-28 11:35:00 -0600

    changed cors origin port to 3000

--- src/main/java/com/equiz/project/controller/GameController.java
+++ src/main/java/com/equiz/project/controller/GameController.java
@@ -14,7 +14,7 @@ import com.equiz.project.service.GameService;
 
 
 @RestController
-@CrossOrigin (origins = {"https://equiz-front.vercel.app:443", "http://localhost:4200"})
+@CrossOrigin (origins = {"https://equiz-front.vercel.app:3000", "http://localhost:4200"})
 public class GameController {
 	
 	@Autowired
