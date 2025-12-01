8efbd235d77c45e16236217a2436483adb59212a
jamesolas <jamesolas@gmail.com>
2025-12-01 09:24:12 -0600

    add 443 port to cors

--- src/main/java/com/equiz/project/controller/GameController.java
+++ src/main/java/com/equiz/project/controller/GameController.java
@@ -14,7 +14,7 @@ import com.equiz.project.service.GameService;
 
 
 @RestController
-@CrossOrigin (origins = {"https://equiz-front.vercel.app", "http://localhost:4200"})
+@CrossOrigin (origins = {"https://equiz-front.vercel.app:443", "http://localhost:4200"})
 public class GameController {
 	
 	@Autowired
